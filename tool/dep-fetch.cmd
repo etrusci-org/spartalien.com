702fd349d3344326e317caa0676bb9e0054ceacc
arT2 <60965610+etrusci-org@users.noreply.github.com>
2022-07-10 01:16:22 +0200

    Janitor.

--- tool/dep-fetch.cmd
+++ tool/dep-fetch.cmd
@@ -6,9 +6,8 @@
 ::curl -o app/protected/lib/vendor/DatabaseSQLite3.php    https://raw.githubusercontent.com/etrusci-org/nifty/main/php/DatabaseSQLite3.php
 ::curl -o app/protected/lib/vendor/jdec.php               https://raw.githubusercontent.com/etrusci-org/nifty/main/php/jdec.php
 ::curl -o app/protected/lib/vendor/jenc.php               https://raw.githubusercontent.com/etrusci-org/nifty/main/php/jenc.php
-:: still dev!
-::curl -o src/ts/vendor/LazyMedia.ts                      https://raw.githubusercontent.com/etrusci-org/nifty/dev/typescript/LazyMedia.ts
-::curl -o src/ts/vendor/addTargetToExtLinks.ts            https://raw.githubusercontent.com/etrusci-org/nifty/dev/typescript/addTargetToExtLinks.ts
+::curl -o src/ts/vendor/LazyMedia.ts                      https://raw.githubusercontent.com/etrusci-org/nifty/main/typescript/LazyMedia.ts
+::curl -o src/ts/vendor/addTargetToExtLinks.ts            https://raw.githubusercontent.com/etrusci-org/nifty/main/typescript/addTargetToExtLinks.ts
 
 :: etrusci-org/scur
 ::curl -o src/ts/vendor/scur.ts                           https://raw.githubusercontent.com/etrusci-org/scur/main/src/scur.ts
