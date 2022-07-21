fa8889d0da1994f4f9af3a27d1feb69f5a455556
arT2 <60965610+etrusci-org@users.noreply.github.com>
2022-07-21 15:52:15 +0200

    Update dep-fetch.

--- tool/dep-fetch.cmd
+++ tool/dep-fetch.cmd
@@ -1,14 +1,11 @@
 @echo off
 
 :: etrusci-org/nifty
-::curl -o app/protected/lib/vendor/WebApp.php             https://raw.githubusercontent.com/etrusci-org/nifty/main/php/WebApp.php
-::curl -o app/protected/lib/vendor/WebRouter.php          https://raw.githubusercontent.com/etrusci-org/nifty/main/php/WebRouter.php
-::curl -o app/protected/lib/vendor/DatabaseSQLite3.php    https://raw.githubusercontent.com/etrusci-org/nifty/main/php/DatabaseSQLite3.php
-::curl -o app/protected/lib/vendor/jdec.php               https://raw.githubusercontent.com/etrusci-org/nifty/main/php/jdec.php
-::curl -o app/protected/lib/vendor/jenc.php               https://raw.githubusercontent.com/etrusci-org/nifty/main/php/jenc.php
-::curl -o src/ts/vendor/LazyMedia.ts                      https://raw.githubusercontent.com/etrusci-org/nifty/main/typescript/LazyMedia.ts
-::curl -o src/ts/vendor/addTargetToExtLinks.ts            https://raw.githubusercontent.com/etrusci-org/nifty/main/typescript/addTargetToExtLinks.ts
-
-:: etrusci-org/scur
-::curl -o src/ts/vendor/scur.ts                           https://raw.githubusercontent.com/etrusci-org/scur/main/src/scur.ts
-::curl -o src/ts/vendor/scur.types.d.ts                   https://raw.githubusercontent.com/etrusci-org/scur/main/src/scur.types.d.ts
+curl -o app/protected/lib/vendor/WebApp.php             https://raw.githubusercontent.com/etrusci-org/nifty/main/php/WebApp.php
+curl -o app/protected/lib/vendor/WebRouter.php          https://raw.githubusercontent.com/etrusci-org/nifty/main/php/WebRouter.php
+curl -o app/protected/lib/vendor/DatabaseSQLite3.php    https://raw.githubusercontent.com/etrusci-org/nifty/main/php/DatabaseSQLite3.php
+curl -o app/protected/lib/vendor/jsonDecode.php         https://raw.githubusercontent.com/etrusci-org/nifty/main/php/jsonDecode.php
+curl -o app/protected/lib/vendor/jsonEncode.php         https://raw.githubusercontent.com/etrusci-org/nifty/main/php/jsonEncode.php
+curl -o src/ts/vendor/LazyMedia.ts                      https://raw.githubusercontent.com/etrusci-org/nifty/main/typescript/LazyMedia.ts
+curl -o src/ts/vendor/addTargetToExtLinks.ts            https://raw.githubusercontent.com/etrusci-org/nifty/main/typescript/addTargetToExtLinks.ts
+curl -o src/ts/vendor/Scur.ts                           https://raw.githubusercontent.com/etrusci-org/nifty/main/typescript/Scur.ts
