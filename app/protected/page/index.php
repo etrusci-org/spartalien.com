852625e2648ecf55605fe7429c935d33c503e116
arT2 <60965610+etrusci-org@users.noreply.github.com>
2022-07-25 15:34:33 +0200

    I had to.

--- app/protected/page/index.php
+++ app/protected/page/index.php
@@ -2,10 +2,29 @@
     <div class="randomQuoteTyper">
         <div class="primeMirror">
             <?php
-            $n = range(10, 100_000);
-            $p = $n[array_rand($n)];
-            $m = strrev($p);
-            printf('%1$s<span class="code">|</span>%2$s', $p, $m);
+            function isPrime(int $num): bool {
+                if ($num < 2) return false;
+                if ($num == 2) return true;
+                if ($num % 2 == 0) return false;
+                $divLast = round($num ** 0.5) + 1;
+                $divCurrent = 3;
+                while ($divCurrent <= $divLast) {
+                    if ($num % $divCurrent == 0) return false;
+                    $divCurrent += 2;
+                }
+                return true;
+            }
+            $numRange = range(10, 100_000);
+            $num = $numRange[array_rand($numRange)];
+            $numx = strrev($num);
+            printf('
+                <span class="code">%3$s=</span>%1$s<span class="code">||</span>%2$s<span class="code">=%4$s</span>
+                ',
+                $num,
+                $numx,
+                (isPrime($num)) ? 'p' : 'n',
+                (isPrime($numx)) ? 'p' : 'n',
+            );
             ?>
         </div>
     </div>
