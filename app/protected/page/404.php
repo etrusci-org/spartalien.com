1a3b8b9f0425492e9c5761ac4f797b5f691c15ca
arT2 <60965610+etrusci-org@users.noreply.github.com>
2022-08-04 14:35:48 +0200

    Update Scur.

--- app/protected/page/404.php
+++ app/protected/page/404.php
@@ -6,7 +6,7 @@
         is invalid.
     </p>
     <p>
-        Feel free to <a data-scur="tFWasR3b6kmbm9GQzBXYyRXYslWZu5yYv12N0UDZzIDOlRmM3cDN2MWY4gDMzMGNiFWM1cTMkR2NzEDNxgzM2UjZ2cjZlRTMjdTYklzN2UTO4EjZjF2Y2EDO">send me<noscript> (JavaScript required)</noscript></a> the following code if you think this is a bug.
+        Feel free to <a data-scur="51|81|84|78|107|78|106|77|52|85|71|90|121|99|122|78|48|89|122|89|104|104|68|79|119|77|122|89|48|73|87|89|120|85|122|78|120|81|71|90|51|77|84|77|48|69|68|79|122|89|84|78|109|90|122|78|109|86|71|78|120|77|50|78|104|82|87|79|51|89|84|78|53|103|84|77|109|78|87|89|106|90|84|77|52|48|87|89|112|120|71|100|118|112|84|97|117|90|50|98|65|78|72|99|104|74|72|100|104|120|87|97|108|53|109|76|106|57|87|98">send me<noscript> (JavaScript required)</noscript></a> the following code if you think this is a bug.
     </p>
 
     <p>
