6dc0255e43f648dc6196632d118dbb9c1dacca78
arT2 <60965610+etrusci-org@users.noreply.github.com>
2022-08-24 15:40:00 +0200

    Replaced CloudcastFetcher with MixcloudData.

--- tool/dep-fetch.cmd
+++ tool/dep-fetch.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-:: etrusci-org/nifty
+:: etrusci-org/nifty/main
 curl -o app/protected/lib/vendor/WebApp.php             https://raw.githubusercontent.com/etrusci-org/nifty/main/php/WebApp.php
 curl -o app/protected/lib/vendor/WebRouter.php          https://raw.githubusercontent.com/etrusci-org/nifty/main/php/WebRouter.php
 curl -o app/protected/lib/vendor/DatabaseSQLite3.php    https://raw.githubusercontent.com/etrusci-org/nifty/main/php/DatabaseSQLite3.php
@@ -9,3 +9,6 @@ curl -o app/protected/lib/vendor/jsonEncode.php         https://raw.githubuserco
 curl -o src/ts/vendor/LazyMedia.ts                      https://raw.githubusercontent.com/etrusci-org/nifty/main/typescript/LazyMedia.ts
 curl -o src/ts/vendor/addTargetToExtLinks.ts            https://raw.githubusercontent.com/etrusci-org/nifty/main/typescript/addTargetToExtLinks.ts
 curl -o src/ts/vendor/Scur.ts                           https://raw.githubusercontent.com/etrusci-org/nifty/main/typescript/Scur.ts
+
+:: etrusci-org/nifty/dev
+curl -o app/protected/lib/vendor/MixcloudData.php       https://raw.githubusercontent.com/etrusci-org/nifty/dev/php/MixcloudData.php
