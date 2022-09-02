6d96e21e2edde032fa9e5bcf32dc2bb00b7042f2
arT2 <60965610+etrusci-org@users.noreply.github.com>
2022-09-02 10:43:43 +0200

    Update privacy.php

--- app/protected/page/privacy.php
+++ app/protected/page/privacy.php
@@ -1,14 +1,19 @@
 <div class="box">
     <h2>PRIVACY ON SPARTALIEN.COM</h2>
-    <p>Last Update: 2022-09-01</p>
+    <p>
+        Last Update: 2022-09-02.
+        Feel free to
+        <a data-scur="51|81|84|78|107|78|106|77|52|85|71|90|121|99|122|78|48|89|122|89|104|104|68|79|119|77|122|89|48|73|87|89|120|85|122|78|120|81|71|90|51|77|84|77|48|69|68|79|122|89|84|78|109|90|122|78|109|86|71|78|120|77|50|78|104|82|87|79|51|89|84|78|53|103|84|77|109|78|87|89|106|90|84|77|52|48|87|89|112|120|71|100|118|112|84|97|117|90|50|98|65|78|72|99|104|74|72|100|104|120|87|97|108|53|109|76|106|57|87|98">send me an email<noscript> (JavaScript required)</noscript></a>
+        if you have any questions.
+    </p>
 </div>
 
 
 <div class="box">
     <h3>SERVER LOGS</h3>
     <p>
-        Server access logs are used to analyze the traffic.
-        This includes your IP, User Agent and Referer website.
+        Server access logs are used to analyze the website traffic.
+        These include your IP, User Agent and Referer website.
         A log entry of you may look like this:<br>
         <span class="code">11.22.33.44 - - [17/Aug/2022:08:49:28 +0000] "GET /music/id:29 HTTP/1.1" 200 3352 "https://spartalien.com/news/id:53" "Mozilla/5.0 (Linux; Android 7.0;)"</span>
     </p>
