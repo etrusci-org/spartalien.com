6ff6a38ddcb81ae0cc026af07bbfabb96eaf7260
arT2 <60965610+etrusci-org@users.noreply.github.com>
2022-09-03 17:16:58 +0200

    Cooler random track on index page.

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -7,9 +7,46 @@ import { ImagePreview } from './ImagePreview.js'
 export const App: AppInterface = {
     main(routeRequest = '') {
         console.log(`SPARTALIEN.COM${(routeRequest) ? ` :: ${routeRequest}` : ``}`)
+
+        // index page
+        if (routeRequest == '') {
+            // @ts-ignore: trackList is defined in app/protected/page/index.php
+            if (trackList) {
+                let randomAudioTarget: HTMLDivElement|null = document.querySelector('.random-audio') || null
+
+                setTimeout(() => {
+                    this.loadRandomTrack(randomAudioTarget)
+                }, 3_000)
+            }
+        }
+
         LazyMedia.embed()
         Scur.deobElements()
-        addTargetToExtLinks()
         ImagePreview.init()
+        addTargetToExtLinks()
     },
+
+    loadRandomTrack(target) {
+        // @ts-ignore: trackList is defined in app/protected/page/index.php
+        if (!target || !trackList) return
+        // @ts-ignore
+        let randomTrack = trackList[Math.floor(Math.random() * trackList.length)]
+
+        if (!randomTrack) return
+
+        target.innerHTML = `<em>Not sure where to start?</em>`
+
+        setTimeout(() => {
+            target.innerHTML += `
+            <em>Here's a random track:</em><br>
+            <br>
+            <h2><a href="${randomTrack.releaseRoute}">${randomTrack.audioName}</a></h2>
+            <p>
+                from the ${randomTrack.releaseType}
+                <a href="${randomTrack.releaseRoute}">${randomTrack.releaseName}</a>
+            </p>
+            <iframe class="lazymedia bandcampTrack small" src="https://bandcamp.com/EmbeddedPlayer/track=${randomTrack.bandcampID}/size=small/bgcol=2b2b2b/linkcol=cccccc/artwork=true/transparent=true/"></iframe>
+            `
+        }, 3_000)
+    }
 }
