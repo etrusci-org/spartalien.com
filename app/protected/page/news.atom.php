e2f1c2c62daafb46013cb16013039368e82452b7
arT2 <60965610+etrusci-org@users.noreply.github.com>
2022-09-27 13:14:29 +0200

    Update news.atom.php

--- app/protected/page/news.atom.php
+++ app/protected/page/news.atom.php
@@ -17,12 +17,24 @@ print('<?xml version="1.0" encoding="utf-8"?>'.PHP_EOL);
     <rights>Copyright (c) 2016-<?php print(date('Y')); ?> SPARTALIEN.COM</rights>
     <?php
     foreach ($news as $v) {
-        $items = implode(" + ", array_map(function(string $v): string {
+        $items = implode(' + ', array_map(function(string $v): string {
             return $this->parseLazyInput($v);
         }, $v['items']));
-        printf('<entry><title>NEWS FROM %3$s</title><link href="%2$s%1$s"/><id>tag:spartalien.com,%3$s:news-%1$s</id><updated>%3$sT00:00:00Z</updated><author><name>SPARTALIEN</name></author><content>%4$s</content></entry>',
+        printf('
+            <entry>
+                <title>NEWS FROM %3$s</title>
+                <link href="%2$s"/>
+                <id>tag:spartalien.com,%3$s:news-%1$s</id>
+                <updated>%3$sT00:00:00Z</updated>
+                <author>
+                    <name>SPARTALIEN</name>
+                </author>
+                <content>
+                    %4$s
+                </content>
+            </entry>',
             $v['id'],
-            sprintf('%1$s%2$s', $this->conf['baseURL'], $this->routeURL('news/id:')),
+            sprintf('%1$s%2$s', $this->conf['baseURL'], $this->routeURL('news/id:%s', [$v['id']])),
             $v['postedOn'],
             htmlspecialchars(strip_tags($items), ENT_HTML5),
         );
