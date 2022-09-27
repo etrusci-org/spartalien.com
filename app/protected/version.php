2671eebad48fb2852ee29d74d4c8278d723e7752
arT2 <60965610+etrusci-org@users.noreply.github.com>
2022-09-27 12:41:05 +0200

    Update version.php

--- app/protected/version.php
+++ app/protected/version.php
@@ -6,11 +6,12 @@ define('VERSION', [
     'favicon' => filemtime('favicon.ico'),
     'og' => filemtime('res/og.jpg'),
     'css' => filemtime('res/style.min.css'),
-    'js' =>
-        array_sum(array_map(function(string $v): int {
+    'js' => array_sum(
+        array_map(function(string $v): int {
             return filemtime($v);
         }, array_merge(
             glob('res/*.js'),
             glob('res/vendor/*.js'),
-        ))),
+        ))
+    ),
 ]);
