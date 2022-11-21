7f921a00647d16113e99a764cc54ae6565d1a99a
arT2 <60965610+etrusci-org@users.noreply.github.com>
2022-11-21 17:35:18 +0100

    Release prep

--- app/protected/page/index.php
+++ app/protected/page/index.php
@@ -1,39 +1,21 @@
-<?php
-$trackList = [];
-
-$releaseList = $this->getAudio('releaseList');
-foreach ($releaseList as $k => $v) {
-    if (!$v['releasedOn']) {
-        continue;
-    }
-
-    $trackList = array_merge($trackList, array_map(function(array $t) use ($v): array {
-        $t = [
-            'audioID' => $t['id'],
-            'audioName' => $t['audioName'],
-            'bandcampID' => $t['bandcampID'],
-            'releaseType' => $v['releaseType'],
-            'releaseName' => $v['releaseName'],
-            'releaseRoute' => $this->routeURL('music/id:%1$s', [$v['id']]),
-        ];
-        return $t;
-    }, $this->getAudioByID($v['audioIDs'])));
-}
-
-$trackList = jsonEncode($trackList, JSON_THROW_ON_ERROR | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
-?>
-
-
-
-
 <div class="text-align-center">
-    <div class="random-audio"></div>
+    <h2>NEW MUSIC ON 23. NOVEMBER 2022 !</h2>
+    <h3><em>OKAY, BUT CAN YOU RAP TO THIS?</em></h3>
+    <p>Finally. <a href="<?php print($this->conf['elsewhere']['newsletter'][1]); ?>" title="<?php print($this->conf['elsewhere']['newsletter'][0]); ?>">Don't miss it</a>! ;-)</p>
+    <p><img src="file/cover/34-med.jpg" alt="cover art"></p>
+    <?php if (date('Ymd') <= '20221123'): ?>
+        <div class="videobox">
+            <span class="lazycode">{
+                "type": "youtubeVideo",
+                "slug": "MckD1G9BZ90"
+            }</span>
+        </div>
+    <?php else: ?>
+        <span class="lazycode">{
+            "type": "spotifyAlbum",
+            "slug": "4CXDCmlFrKxbmQjkEINHQa",
+            "trackCount": "10",
+            "disableTheme": true
+        }</span>
+    <?php endif; ?>
 </div>
-
-
-
-
-
-<script>
-    const trackList = <?php print($trackList.PHP_EOL); ?>
-</script>
