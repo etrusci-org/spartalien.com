561a92dbbf6795fd5f018953a62b5818a165ca67
arT2 <60965610+etrusci-org@users.noreply.github.com>
2022-11-23 00:29:48 +0100

    Post release

--- app/protected/page/index.php
+++ app/protected/page/index.php
@@ -1,10 +1,26 @@
 <div class="text-align-center">
     <h2>NEW MUSIC !</h2>
-    <h3><em>OKAY, BUT CAN YOU RAP TO THIS?</em></h3>
+
+    <h3>"<a href="music/id:34">OKAY, BUT CAN YOU RAP TO THIS?</a>"</h3>
     <p>
-        <a href="music/id:34">
-            LISTEN NOW !<br>
-            <img src="file/cover/34-med.jpg" alt="cover art">
-        </a>
+        <a href="music/id:34">LISTEN HERE OR ON SPOTIFY AND MOST MUSIC PLATFORMS!</a>
     </p>
+
+    <div class="lazycode">{
+        "type": "spotifyAlbum",
+        "slug": "4CXDCmlFrKxbmQjkEINHQa",
+        "trackCount": 9,
+        "disableTheme": true
+    }</div>
+
+    <div class="lazycode">{
+        "type": "bandcampAlbum",
+        "slug": "4047724916",
+        "trackCount": 9
+    }</div>
+
+    <p>
+        <a href="music/id:34"><img src="file/cover/34-med.jpg" alt="cover art"></a>
+    </p>
+
 </div>
