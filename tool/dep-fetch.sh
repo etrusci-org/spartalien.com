cb49a3cada9a66ed4a39889e9baba8985eef3900
arT2 <60965610+etrusci-org@users.noreply.github.com>
2022-12-18 12:56:12 +0100

    Update deps

--- tool/dep-fetch.sh
+++ tool/dep-fetch.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 # etrusci-org/nifty
-curl -o ./app/protected/lib/vendor/WebApp.php             https://raw.githubusercontent.com/etrusci-org/nifty/f679e49bbb2bc2f7f98024ebb929775f86c600ab/php/WebApp.php
-curl -o ./app/protected/lib/vendor/WebRouter.php          https://raw.githubusercontent.com/etrusci-org/nifty/f679e49bbb2bc2f7f98024ebb929775f86c600ab/php/WebRouter.php
-curl -o ./app/protected/lib/vendor/DatabaseSQLite3.php    https://raw.githubusercontent.com/etrusci-org/nifty/f679e49bbb2bc2f7f98024ebb929775f86c600ab/php/DatabaseSQLite3.php
-curl -o ./app/protected/lib/vendor/MixcloudData.php       https://raw.githubusercontent.com/etrusci-org/nifty/f679e49bbb2bc2f7f98024ebb929775f86c600ab/php/MixcloudData.php
-curl -o ./app/protected/lib/vendor/jsonDecode.php         https://raw.githubusercontent.com/etrusci-org/nifty/f679e49bbb2bc2f7f98024ebb929775f86c600ab/php/jsonDecode.php
-curl -o ./app/protected/lib/vendor/jsonEncode.php         https://raw.githubusercontent.com/etrusci-org/nifty/f679e49bbb2bc2f7f98024ebb929775f86c600ab/php/jsonEncode.php
-curl -o ./src/ts/vendor/LazyMedia.ts                      https://raw.githubusercontent.com/etrusci-org/nifty/f679e49bbb2bc2f7f98024ebb929775f86c600ab/typescript/LazyMedia.ts
-curl -o ./src/ts/vendor/addTargetToExtLinks.ts            https://raw.githubusercontent.com/etrusci-org/nifty/f679e49bbb2bc2f7f98024ebb929775f86c600ab/typescript/addTargetToExtLinks.ts
-curl -o ./src/ts/vendor/Scur.ts                           https://raw.githubusercontent.com/etrusci-org/nifty/f679e49bbb2bc2f7f98024ebb929775f86c600ab/typescript/Scur.ts
+curl -o ./app/protected/lib/vendor/WebApp.php             https://raw.githubusercontent.com/etrusci-org/nifty/1076f41f8c1bdb8970e729d10b2d37b0fb1908d3/php/WebApp.php
+curl -o ./app/protected/lib/vendor/WebRouter.php          https://raw.githubusercontent.com/etrusci-org/nifty/1076f41f8c1bdb8970e729d10b2d37b0fb1908d3/php/WebRouter.php
+curl -o ./app/protected/lib/vendor/DatabaseSQLite3.php    https://raw.githubusercontent.com/etrusci-org/nifty/1076f41f8c1bdb8970e729d10b2d37b0fb1908d3/php/DatabaseSQLite3.php
+curl -o ./app/protected/lib/vendor/MixcloudData.php       https://raw.githubusercontent.com/etrusci-org/nifty/1076f41f8c1bdb8970e729d10b2d37b0fb1908d3/php/MixcloudData.php
+curl -o ./app/protected/lib/vendor/jsonDecode.php         https://raw.githubusercontent.com/etrusci-org/nifty/1076f41f8c1bdb8970e729d10b2d37b0fb1908d3/php/jsonDecode.php
+curl -o ./app/protected/lib/vendor/jsonEncode.php         https://raw.githubusercontent.com/etrusci-org/nifty/1076f41f8c1bdb8970e729d10b2d37b0fb1908d3/php/jsonEncode.php
+curl -o ./src/ts/vendor/LazyMedia.ts                      https://raw.githubusercontent.com/etrusci-org/nifty/1076f41f8c1bdb8970e729d10b2d37b0fb1908d3/typescript/LazyMedia.ts
+curl -o ./src/ts/vendor/addTargetToExtLinks.ts            https://raw.githubusercontent.com/etrusci-org/nifty/1076f41f8c1bdb8970e729d10b2d37b0fb1908d3/typescript/addTargetToExtLinks.ts
+curl -o ./src/ts/vendor/Scur.ts                           https://raw.githubusercontent.com/etrusci-org/nifty/1076f41f8c1bdb8970e729d10b2d37b0fb1908d3/typescript/Scur.ts
