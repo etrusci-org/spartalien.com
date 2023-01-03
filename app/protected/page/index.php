9d2611d345d994ab4c8c2977eaa51b00e303c1ed
arT2 <60965610+etrusci-org@users.noreply.github.com>
2023-01-03 15:48:02 +0100

    Cooler index page

--- app/protected/page/index.php
+++ app/protected/page/index.php
@@ -1,26 +1,98 @@
-<div class="text-align-center">
-    <h2>NEW MUSIC !</h2>
+<?php
+preg_match('/music\/id\:\[\d\-(\d+)\]/', $this->conf['validRequestPatterns'][2], $match);
+$latestAudioReleaseID = ($match) ? $match[1] : null;
+if ($latestAudioReleaseID) {
+    printf('<script>const latestAudioReleaseID = %d;</script>', $latestAudioReleaseID);
+}
+?>
 
-    <h3>"<a href="music/id:34">OKAY, BUT CAN YOU RAP TO THIS?</a>"</h3>
-    <p>
-        <a href="music/id:34">LISTEN HERE OR ON SPOTIFY AND MOST MUSIC PLATFORMS!</a>
-    </p>
 
-    <div class="lazycode">{
-        "type": "spotifyAlbum",
-        "slug": "4CXDCmlFrKxbmQjkEINHQa",
-        "trackCount": 9,
-        "disableTheme": true
-    }</div>
+<div class="grid two text-align-center">
 
-    <div class="lazycode">{
-        "type": "bandcampAlbum",
-        "slug": "4047724916",
-        "trackCount": 9
-    }</div>
+    <div>
+        <p>random music release... <small>(<a href="#" class="random-audio-reload">reload</a>)</small></p>
+        <noscript><div class="noscript">JavaScript required</div></noscript>
+        <div class="random-audio-target" style="background-image: url(res/loading.gif); background-position: 50% 50%; background-repeat: no-repeat; background-size: 1rem;"></div>
+    </div>
 
-    <p>
-        <a href="music/id:34"><img src="file/cover/34-med.jpg" alt="cover art"></a>
-    </p>
+    <div>
+        <p>join insider club...</p>
+        <a href="http://eepurl.com/dqYlHr">
+            <img src="res/newsletter-qr.png" alt="qrcode">
+        </a>
+    </div>
 
+    <div>
+        <p>elsewhere...</p>
+        <?php
+        foreach ($this->conf['elsewhere'] as $v) {
+            if (strtolower($v[0]) == 'newsletter') continue;
+            printf(
+                '
+                <a class="btn" href="%2$s">%1$s</a>',
+                $v[0],
+                $v[1],
+            );
+        }
+        ?>
+    </div>
+
+    <div>
+        <p>random quote... <small>(<a href="#" class="random-quote-reload">reload</a>)</small></p>
+        <noscript><div class="noscript">JavaScript required</div></noscript>
+        <div class="random-quote-target"></div>
+    </div>
 </div>
+
+
+<script type="module">
+    import { RandomQuoteTyper } from "./res/vendor/RandomQuoteTyper.js"
+    import { quotes } from 'https://cdn.jsdelivr.net/gh/etrusci-org/quotes@main/js/quotes-s9.min.js'
+
+    let randomAudioReload = document.querySelector('.random-audio-reload')
+    let randomQuoteReload = document.querySelector('.random-quote-reload')
+
+    function loadRandomAudioRelease() {
+        let randomAudioTarget = document.querySelector('.random-audio-target')
+
+        if (randomAudioTarget && latestAudioReleaseID) {
+            let randomAudioReleaseID = Math.floor((Math.random() * latestAudioReleaseID) + 1)
+
+            let e1 = document.createElement('a')
+            e1.setAttribute('href', `music/id:${randomAudioReleaseID}`)
+
+            let e2 = document.createElement('img')
+            e2.setAttribute('src', `file/cover/${randomAudioReleaseID}-med.jpg`)
+            e2.setAttribute('alt', 'coverart')
+
+            e1.append(e2)
+            randomAudioTarget.innerHTML = ''
+            randomAudioTarget.append(e1)
+        }
+    }
+
+    loadRandomAudioRelease()
+
+    randomAudioReload.addEventListener('click', (event) => {
+        event.preventDefault()
+        loadRandomAudioRelease()
+    }, false)
+
+    // ---------------------------------------------------------------------------------
+
+    RandomQuoteTyper.targetSelector = '.random-quote-target'
+
+    if (quotes) {
+        RandomQuoteTyper.init(quotes)
+        RandomQuoteTyper.typeQuote()
+    }
+
+    setInterval(() => {
+        RandomQuoteTyper.typeQuote()
+    }, 600_000)
+
+    randomQuoteReload.addEventListener('click', (event) => {
+        event.preventDefault()
+        RandomQuoteTyper.typeQuote()
+    }, false)
+</script>
