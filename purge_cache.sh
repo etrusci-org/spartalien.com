0d403266d3f6f2aed83d4c09475eb69f7609602c
arT2 <60965610+etrusci-org@users.noreply.github.com>
2023-10-07 12:27:35 +0200

    Less scary purge_cache.sh

--- purge_cache.sh
+++ purge_cache.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-rm -fv ./app/cache/*
+rm -v ./app/cache/cached_route_*.php
+rm -v ./app/cache/compiled_route_*.php
+rm -v ./app/cache/mixcloud-lowtechman-cloudcasts.json
+rm -v ./app/cache/mixcloud-lowtechman-user.json
+rm -v ./app/cache/valid_requests.php
+rm -v ./app/cache/version.php
