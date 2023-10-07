cc293e76803ac000401d5f161d09da23f3caef53
arT2 <60965610+etrusci-org@users.noreply.github.com>
2023-10-07 12:29:02 +0200

    -f

--- purge_cache.sh
+++ purge_cache.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-rm -v ./app/cache/cached_route_*.php
-rm -v ./app/cache/compiled_route_*.php
-rm -v ./app/cache/mixcloud-lowtechman-cloudcasts.json
-rm -v ./app/cache/mixcloud-lowtechman-user.json
-rm -v ./app/cache/valid_requests.php
-rm -v ./app/cache/version.php
+rm -fv ./app/cache/cached_route_*.php
+rm -fv ./app/cache/compiled_route_*.php
+rm -fv ./app/cache/mixcloud-lowtechman-cloudcasts.json
+rm -fv ./app/cache/mixcloud-lowtechman-user.json
+rm -fv ./app/cache/valid_requests.php
+rm -fv ./app/cache/version.php
