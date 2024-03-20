132b59de1a863801db123d71acc69659f086b801
arT2 <60965610+etrusci-org@users.noreply.github.com>
2024-03-20 22:30:49 +0100

    Privacy text

--- app/page/privacy.php
+++ app/page/privacy.php
@@ -3,7 +3,7 @@
 
 <div class="box">
     <p>
-        Last Update: 2023-04-22.
+        Last Update: 2024-03-20.
         Feel free to
         <a data-scur="61|61|103|78|105|86|84|77|53|77|87|77|107|49|121|89|104|86|84|77|116|81|84|82|48|73|84|76|53|77|50|89|108|49|105|90|109|82|71|78|104|74|68|79|105|66|84|90|106|66|106|75|48|85|85|79|68|100|122|77|119|77|84|76|69|74|87|90|48|48|67|78|105|90|106|78|116|69|69|79|109|82|87|76|67|70|84|78|122|85|68|82|51|89|85|81|121|81|85|82|107|89|122|89|50|77|106|89|107|100|106|78|116|85|68|78|105|104|84|76|48|73|71|78|104|49|83|89|122|107|84|78|116|85|69|82|122|69|68|77|70|70|84|78|69|90|69|79|121|48|87|89|112|120|71|100|118|112|84|97|117|90|50|98|65|78|72|99|104|74|72|100|104|120|87|97|108|53|109|76|106|57|87|98">send me an email</a> <noscript>[Javascript required]</noscript>
         if you have any questions.
@@ -13,16 +13,17 @@
 
 <div class="box">
     <h3>Webserver Logs</h3>
-    <p>Server access logs are used to analyze the website traffic. These include your IP, User Agent and Referer website. A log entry of you may look like this:</p>
+    <p>Server access logs are utilized for analyzing website traffic. These logs contain information such as your IP address, User Agent, and Referer website. A typical log entry may appear as follows:</p>
     <p><code>11.22.33.44 - - [17/Aug/2022:08:49:28 +0000] "GET /music/id:29 HTTP/1.1" 200 3352 "https://spartalien.com/news/id:53" "Mozilla/5.0 (Linux; Android 7.0;)"</code></p>
+    <p>Additionally, certain errors are logged separately. For instance, when encountering a 404 error (page not found), the logs would resemble this:</p>
+    <p><code>1710231337.0037 | 2024-03-12 08:15:37 UTC | error 404 | /some/page | client_ip_hash=HASH_CODE | client_agent=Mozilla/5.0</code></p>
 </div>
 
 
 <div class="box">
     <h3>Trackers</h3>
-    <p>This website does not make use of third-party trackers.</p>
-    <p>Embedded content (e.g. Music and Video players) can temporarily introduce some on the pages they are loaded.</p>
-    <p>In the <a href="//eepurl.com/dqYlHr">Newsletter</a> mailinglist, opens and clicks get tracked.</p>
+    <p>My website refrains from employing third-party trackers. However, it's important to note that embedded content, such as music and video players, may introduce tracking elements on the pages where they are loaded. These trackers provide various statistics, which I utilize.</p>
+    <p>Furthermore, in my <a href="//eepurl.com/dqYlHr">Newsletter</a> mailing list, opens and clicks are tracked.</p>
 </div>
 
 
