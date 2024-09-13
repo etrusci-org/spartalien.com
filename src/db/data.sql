292d2d4bb950c2fca2ac11df5259cbdf53f34bca
arT2 <60965610+etrusci-org@users.noreply.github.com>
2024-09-13 12:06:26 +0200

    new link

--- src/db/data.sql
+++ src/db/data.sql
@@ -1305,6 +1305,7 @@ INSERT INTO exit (id, text, url, description) VALUES (53, 'Käthi''s Atelier', '
 INSERT INTO exit (id, text, url, description) VALUES (54, 'GifCities', '//gifcities.org', NULL);
 INSERT INTO exit (id, text, url, description) VALUES (55, 'DeinVet', '//dein-vet.ch', NULL);
 INSERT INTO exit (id, text, url, description) VALUES (56, 'Playnite', '//playnite.link', NULL);
+INSERT INTO exit (id, text, url, description) VALUES (57, 'Sounding Soil', '//soundingsoil.ch', NULL);
 
 INSERT INTO p420_session(num, pub_date, runtime, mixcloud_key) VALUES (1, '2021-05-04', 10370, NULL);
 INSERT INTO p420_session(num, pub_date, runtime, mixcloud_key) VALUES (2, '2021-05-06', 10371, 'lowtechman/planet-4202');
